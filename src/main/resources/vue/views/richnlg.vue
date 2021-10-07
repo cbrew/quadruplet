2ef0e39362c6a89ac65185654f89c94900f6f668
cbrew <cbrew@liveperson.com>
2021-10-07 08:56:06 -0400

    rebuilding

---
+++ src/main/resources/vue/views/richnlg.vue
@@ -0,0 +1,25 @@
+<template id="richnlg">
+
+
+    <div class="card text-center m-3">
+        <h5 class="card-header">{{name}}</h5>
+        <input style="width:500px"
+
+               v-model="msg">
+        <div class="card-body">{{msg}} </div>
+    </div>
+</template>
+
+<script>
+var p = Vue.component("richnlg", {
+  name: "post-request",
+  template: "#richnlg",
+  data: () => ({
+           msg:"Beth Ann has an umbrella",
+           name: "Rich NLU"
+        }),
+  created() {
+
+  },
+  });
+</script>
