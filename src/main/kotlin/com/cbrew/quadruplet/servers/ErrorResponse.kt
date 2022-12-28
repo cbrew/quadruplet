ed9b48ef4c1b2e8834a4f179cc62875539a2ae9a
Chris Brew <brewc@dy6nm2ng4q.legal.regn.net>
2022-12-28 08:04:48 -0500

    remove api server

--- src/main/kotlin/com/cbrew/quadruplet/servers/ErrorResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.cbrew.quadruplet.servers
-
-
-data class ErrorResponse(
-    val title: String,
-    val status: Int,
-    val type: String,
-    val details: Map<String, String>?
-)
--- src/main/kotlin/com/cbrew/quadruplet/servers/ResourceResponse.kt
+++
@@ -1,3 +0,0 @@
-package com.cbrew.quadruplet.servers
-
-data class ResourceResponse(val id:Int)
